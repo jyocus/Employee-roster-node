aaee2406a62fac170dada2164edb4bfd2b12a3f7
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-21 16:51:31 -0500

    initial object received from Employee class, package.json added

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+constructor (name, id, email){
+this.name = "justin";
+this.id = 1;
+this.email = "j@j.com"
+}
+
+getName() {
+
+}
+
+getID(){
+
+}
+
+getEmail(){
+
+
+}
+
+getRole(){
+
+}
+
+}
+
+var ob = new Employee();
+console.log(ob);
+
+module.exports = Employee;
