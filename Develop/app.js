e6fd34da29ce138b5077eae9bcffad5409ea319c
Justin Yocus <justin.o.yocus@gmail.com>
2020-12-02 21:03:16 -0600

    fixed functionality and inquirer so the right data would render

--- Develop/app.js
+++ Develop/app.js
@@ -9,9 +9,52 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { type } = require("os");
+
 
 var employeesArr = [] // Empty array that will save all team members 
+
+function initialQuestions() {
+
+    var roleChoices = []
+    roleChoices.push("Add Intern");
+    roleChoices.push("Add Engineer")
+    roleChoices.push("Add Manager");
+    roleChoices.push("I'm Finished");
+
+    inquirer.prompt([{
+        message: "What Employee would you like to add?",
+        name: "role",
+        type: "list",
+        choices: roleChoices
+    }])
+        .then(function (answers) {
+            switch (answers.role) {
+                case "Add Manager": {
+                    managerQuestions();
+                    break;
+                }
+                case "Add Engineer": {
+                    engineerQuestions();
+                    break;
+                }
+                case "Add Intern": {
+                    internQuestions();
+                    break;
+                }
+                case "I'm Finished": {
+                    fs.writeFile(outputPath, render(employeesArr), function (err) {
+
+                        if (err) {
+                            
+                        }
+        
+                        // console.log("Success!");
+                    })
+                    break;
+                }
+            }
+        })
+}
 
 //Inquirer prompts to gather user input - worked with Benjamin Dionysus, Matt Hiatt, and Steve Kavuu on structure
 function managerQuestions() {
@@ -19,7 +62,24 @@ function managerQuestions() {
         [{
 
             message: "Name:",
-            name: "name:",
+            name: "name",
+            type: 'input',
+        },
+        {
+
+            message: "Role:",
+            name: "role",
+            type: 'input',
+
+        },
+        {
+            message: "Email:",
+            name: "email",
+            type: 'input',
+        },
+        {
+            message: "ID:",
+            name: "id",
             type: 'input',
         },
         {
@@ -27,120 +87,85 @@ function managerQuestions() {
             name: "officeNumber",
             type: 'input',
         },
-        {
-            message: "Email:",
-            name: "email",
-            type: 'input',
-        }])
+       ])
         .then(function (answers) {
             employeesArr.push(new Manager(answers.name, employeesArr.length, answers.email, answers.officeNumber));
             initialQuestions();
         });
+}
 
-    function engineerQuestions() {
-        inquirer.prompt(
-            [{
+function engineerQuestions() {
+    inquirer.prompt(
+        [{
 
-                message: "Name:",
-                name: "name",
-                type: 'input',
-            },
-            {
-                message: "GitHub Username:",
-                name: "github",
-                type: 'input',
-            },
-            {
-                message: "Email:",
-                name: "email",
-                type: 'input',
-            }])
-            .then(function (answers) {
-                employeesArr.push(new Engineer(answers.name, employeesArr.length, answers.email, answers.github));
-                initialQuestions();
-            });
-
-        function internQuestions() {
-            inquirer.prompt(
-                [{
-
-                    message: "Name:",
-                    name: "name:",
-                    type: 'input',
-                },
-                {
-                    message: "Name of school:",
-                    name: "school",
-                    type: 'input',
-                },
-                {
-                    message: "Email:",
-                    name: "email",
-                    type: 'input',
-                }])
-                .then(function (answers) {
-                    employeesArr.push(new Intern(answers.name, employeesArr.length, answers.email, answers.school));
-                    initialQuestions();
-                    console.log(answers);
-                });
-
-    // function to initialize program
-    function init() {
-        inquirer
-        .prompt(initialQuestions)
+            message: "Name:",
+            name: "name",
+            type: 'input',
+        },
+        {
+            message: "Role:",
+            name: "role",
+            type: 'input',
+        },
+        {
+            message: "Email:",
+            name: "email",
+            type: 'input',
+        },
+        {
+            message: "ID:",
+            name: "id",
+            type: 'input',
+        },
+        {
+            message: "GitHub Username:",
+            name: "github",
+            type: 'input',
+        }
+        ])
         .then(function (answers) {
-            console.log(response);
-            // function to write html file
-            fs.writeFile(outputPath,render(employeesArr), function (err) {
-    
-                if (err) {
-                    return console.log(err);
-                }
-    
-                // console.log("Success!");
-            })
-        })
-    }
-    
-    // function call to initialize program
-    init();
-        }
-
-function initialQuestions () {
-
-    var roleChoices=[]
-    roleChoices.push("Add Intern");
-    roleChoices.push("Add Engineer")
-    roleChoices.push("Add Manager");
-    roleChoices.push("I'm Finished");
-
-    inquirer.prompt([{
-        message:"What Employee would you like to add?",
-        name: "role",
-        type: "list",
-        choices: roleChoices
-    }])
-    .then(function(answers){
-        switch(answers.role){
-            case "Add Manager": {
-                managerQuestions();
-                break;
-            }
-            case "Add Engineer":{
-                engineerQuestions();
-                break;
-            }
-            case "Add Intern":{
-                internQuestions();
-                break;
-            }
-            case "I'm Finished":{
-                fs.writeFile(employeesArr);
-                break;
-            }
-        }
-    })
+            employeesArr.push(new Engineer(answers.name, employeesArr.length, answers.email, answers.github));
+            initialQuestions();
+        });
 }
-    }
+
+function internQuestions() {
+    inquirer.prompt(
+        [{
+
+            message: "Name:",
+            name: "name",
+            type: 'input',
+        },
+        {
+            message: "Role:",
+            name: "role",
+            type: 'input',
+        },
+        {
+            message: "Email:",
+            name: "email",
+            type: 'input',
+        },
+        {
+            message: "ID:",
+            name: "id",
+            type: 'input',
+        },
+        {
+            message: "School:",
+            name: "school",
+            type: 'input',
+        }])
+        .then(function (answers) {
+            employeesArr.push(new Intern(answers.name, employeesArr.length, answers.email, answers.school));
+            initialQuestions();
+            
+        });
 }
+
+initialQuestions();
+
+
+
 
